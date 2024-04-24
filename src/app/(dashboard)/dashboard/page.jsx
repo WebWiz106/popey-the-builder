9bf231eee90388ddc3756b8ffb14ed809f400088
harshVrdn03 <vk5415308@gmail.com>
2024-04-24 15:58:25 +0530

    client

---
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="">
+      <Link className="btn btn-primary" href={"/"}>
+        Home
+      </Link>
+    </div>
+  );
+}
